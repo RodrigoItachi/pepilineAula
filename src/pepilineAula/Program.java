ae73892eac7928fb4629441c1a37ab7c95eb14f9
RodrigoItachi <sousarodrigo56@outlook.com>
2021-06-05 20:08:29 -0300

    operation final

--- src/pepilineAula/Program.java
+++ src/pepilineAula/Program.java
@@ -2,6 +2,7 @@ package pepilineAula;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Program {
@@ -9,8 +10,18 @@ public class Program {
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
 
-		Stream<Integer> stream1 = list.stream().map(x -> x * 10);//operação intermediária
-		System.out.println("Stream 1: " + Arrays.toString(stream1.toArray()));//operação terminal
+		Stream<Integer> stream1 = list.stream().map(x -> x * 10);// operação intermediária
+		System.out.println("Stream 1: " + Arrays.toString(stream1.toArray()));// operação terminal
+
+		int sum = list.stream().reduce(0, (x, y) -> x + y);
+		System.out.println("Sum list: " + sum);
+
+		List<Integer> list2 = list.stream()
+				.filter(x -> x % 2 == 0)
+				.map(x -> x * 10)
+				.collect(Collectors.toList());
+		
+		System.out.println("List 2: "+Arrays.asList(list2.toArray()));
 	}
 
 }
